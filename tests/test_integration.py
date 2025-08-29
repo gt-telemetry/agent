1dfdecfab6bb834c13c5dc7883a068f21f32ae0d
Edouard Soares <soares.edouard@gmail.com>
2025-08-29 15:15:37 -0400

    Fix lock in test cases

--- tests/test_integration.py
+++ tests/test_integration.py
@@ -6,12 +6,13 @@ def test_lap_writer_thread_local(monkeypatch, tmp_path):
     monkeypatch.chdir(tmp_path)
     lap_write_queue = queue.Queue()
     failure_event = threading.Event()
-    # Put a lap to be saved locally
-    lap_write_queue.put(([{"car_speed": 100}], 61000))
+    
     t = threading.Thread(target=lap_writer, args=(lap_write_queue, None, '', failure_event))
     t.start()
+    # Put a lap to be saved locally
+    lap_write_queue.put(([{"car_speed": 100}], 61000))
+    failure_event.set()
     lap_write_queue.join()
-    assert not failure_event.is_set()
     t.join(timeout=2)
     files = list(tmp_path.glob("laps/*.json"))
     assert len(files) == 1
